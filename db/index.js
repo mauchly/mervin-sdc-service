b9138046c1e4be0fd193b5c1964c157131a1063f
Mervin Pan <mervinpan@gmail.com>
2020-05-16 13:20:40 -0700

    Added new relic and artillery

--- db/index.js
+++ db/index.js
@@ -16,17 +16,17 @@ var pg = require('knex')({
   }
 });
 
-// const getMainRouteString = (id) => {
-//   return new Promise((resolve, reject) => {
-//     let select_query_name = `SELECT * FROM Photos WHERE name='${id}'`;
-//     db.query(select_query_name, (err, results) => {
-//       if (err) {
-//         reject(err);
-//       }
-//       resolve(results);
-//     });
-//   });
-// };
+const getMainRouteString = (id) => {
+  return new Promise((resolve, reject) => {
+    let select_query_name = `SELECT * FROM Photos WHERE name='${id}'`;
+    db.query(select_query_name, (err, results) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(results);
+    });
+  });
+};
 
 const getMainRouteNum = (id) => {
   return new Promise((resolve, reject) => {
@@ -41,41 +41,45 @@ const getMainRouteNum = (id) => {
 
 const toggleFavorite = (id) => {
   return new Promise((resolve, reject) => {
-    let update_query = `UPDATE Photos SET is_favorite = 1 - is_favorite WHERE listing_id=${id}`;
-    db.query(update_query, (err, results) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(results);
-    });
+    pg('favorite').where('listing_id', id)
+      .then(data => {
+        pg.from('favorite').where('listing_id', id).update('favorite', !data[0].favorite)
+          .then(() => {resolve()})
+          .catch(err => {reject(err)})
+      })
+      .catch(err => {reject(err)})
   });
 };
 
 const recPhotos = (id) => {
   return new Promise((resolve, reject) => {
-    let select_query = `SELECT photo1_b, photo2_b, photo3_b, photo4_b, photo5_b, photo6_b, photo7_b, photo8_b, photo9_b, photo10_b, photo11_b, photo12_b, photo13_b, photo14_b, photo15_b, photo16_b, photo17_b, photo18_b, photo19_b, photo20_b, photo21_b, photo22_b, photo23_b, photo24_b, photo25_b, photo26_b, photo27_b, photo28_b, photo29_b, photo30_b FROM Photos WHERE listing_id=${id}`;
-    db.query(select_query, (err, results) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(results);
-    });
+    pg.from('listings').where('listing_id', id)
+      .then(data => {
+        let object = {};
+        data.forEach((each,index) => {
+          object[`photo${index+1}_b`] = each.s_photo;
+        });
+        resolve(object)
+      })
+      .catch(err => {reject(err)})
   });
 };
 
-const deleteListing = () => {
+const deleteListingPhoto = (listingDetails) => {
 
 }
 
-const postListing = () => {
+const postListingPhoto = (listingDetails) => {
 
 }
+
+// postListing(-1)
 
 module.exports = {
   // getMainRouteString,
   getMainRouteNum,
   toggleFavorite,
   recPhotos,
-  deleteListing,
-  postListing,
+  deleteListingPhoto,
+  postListingPhoto,
 };
