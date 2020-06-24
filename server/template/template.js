1a439be4f2f9e8c93dd663c32e8bcf9e25762100
Mervin Pan <mervinpan@gmail.com>
2020-06-23 19:23:29 -0700

    putting script to proxy

--- server/template/template.js
+++ server/template/template.js
@@ -1,7 +1,6 @@
 var template = function(appString) {
   return (
-    `<div id="PhotoService">${appString}</div>
-    <script type="text/javascript" src="http://52.52.28.79/bundle.js" async ></script>`
+    `<div id="PhotoService">${appString}</div>`
   )
 }
 
